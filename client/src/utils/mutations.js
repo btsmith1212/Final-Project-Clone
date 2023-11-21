64ed12793c4633461a9e3adfb3e6d80e5b4a7a1a
Yiseo Kwon <yiseo1122@gmail.com>
2023-11-20 21:27:34 -0600

    Modified add user mutation

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -13,8 +13,8 @@ export const LOGIN_USER = gql `
 `;
 
 export const ADD_USER = gql `
-    mutation addUser($username: String!, $password: String!) {
-        addUser(username: $username, password: $password) {
+    mutation registerUser($username: String!, $password: String!) {
+        registerUser(username: $username, password: $password) {
             token
             user {
                 _id
