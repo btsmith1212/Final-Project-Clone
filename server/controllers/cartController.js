252159453f70a3ed452b0a5ea9c1031077be3d6a
Emily Caton <caton.emily@yahoo.com>
2023-11-21 18:24:20 -0600

    comment out controllers, dont need them, can delete completely later

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -1,104 +1,104 @@
-const Cart = require('../models/Cart');
-const Product = require('../models/Products');
+// const Cart = require('../models/Cart');
+// const Product = require('../models/Products');
 
-const cartController = {
-  // Get the user's cart
-  getCart: async (req, res) => {
-    const userId = req.user.userId; // Assuming you have user information stored in the request (possibly from authentication middleware)
-    try {
-      const userCart = await Cart.findOne({ user: userId }).populate('items.product');
-      res.json(userCart);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
+// const cartController = {
+//   // Get the user's cart
+//   getCart: async (req, res) => {
+//     const userId = req.user.userId; // Assuming you have user information stored in the request (possibly from authentication middleware)
+//     try {
+//       const userCart = await Cart.findOne({ user: userId }).populate('items.product');
+//       res.json(userCart);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
 
-  // Add an item to the cart
-  addItemToCart: async (req, res) => {
-    const userId = req.user.userId;
-    const { productId, quantity } = req.body;
+//   // Add an item to the cart
+//   addItemToCart: async (req, res) => {
+//     const userId = req.user.userId;
+//     const { productId, quantity } = req.body;
 
-    try {
-      const product = await Product.findById(productId);
-      if (!product) {
-        return res.status(404).json({ error: 'Product not found' });
-      }
+//     try {
+//       const product = await Product.findById(productId);
+//       if (!product) {
+//         return res.status(404).json({ error: 'Product not found' });
+//       }
 
-      const userCart = await Cart.findOne({ user: userId });
-      if (!userCart) {
-        // If the user doesn't have a cart, create one
-        const newCart = new Cart({ user: userId, items: [{ product: productId, quantity }] });
-        const savedCart = await newCart.save();
-        return res.status(201).json(savedCart);
-      }
+//       const userCart = await Cart.findOne({ user: userId });
+//       if (!userCart) {
+//         // If the user doesn't have a cart, create one
+//         const newCart = new Cart({ user: userId, items: [{ product: productId, quantity }] });
+//         const savedCart = await newCart.save();
+//         return res.status(201).json(savedCart);
+//       }
 
-      // If the user already has a cart, update the existing cart
-      const existingItem = userCart.items.find((item) => item.product.toString() === productId);
-      if (existingItem) {
-        // If the product is already in the cart, update the quantity
-        existingItem.quantity += quantity;
-      } else {
-        // If the product is not in the cart, add it
-        userCart.items.push({ product: productId, quantity });
-      }
+//       // If the user already has a cart, update the existing cart
+//       const existingItem = userCart.items.find((item) => item.product.toString() === productId);
+//       if (existingItem) {
+//         // If the product is already in the cart, update the quantity
+//         existingItem.quantity += quantity;
+//       } else {
+//         // If the product is not in the cart, add it
+//         userCart.items.push({ product: productId, quantity });
+//       }
 
-      const updatedCart = await userCart.save();
-      res.json(updatedCart);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
+//       const updatedCart = await userCart.save();
+//       res.json(updatedCart);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
 
-  // Update quantity of an item in the cart
-  updateCartItemQuantity: async (req, res) => {
-    const userId = req.user.userId;
-    const { productId, quantity } = req.body;
+//   // Update quantity of an item in the cart
+//   updateCartItemQuantity: async (req, res) => {
+//     const userId = req.user.userId;
+//     const { productId, quantity } = req.body;
 
-    try {
-      const userCart = await Cart.findOne({ user: userId });
-      if (!userCart) {
-        return res.status(404).json({ error: 'Cart not found' });
-      }
+//     try {
+//       const userCart = await Cart.findOne({ user: userId });
+//       if (!userCart) {
+//         return res.status(404).json({ error: 'Cart not found' });
+//       }
 
-      const existingItem = userCart.items.find((item) => item.product.toString() === productId);
-      if (!existingItem) {
-        return res.status(404).json({ error: 'Item not found in the cart' });
-      }
+//       const existingItem = userCart.items.find((item) => item.product.toString() === productId);
+//       if (!existingItem) {
+//         return res.status(404).json({ error: 'Item not found in the cart' });
+//       }
 
-      // Update the quantity
-      existingItem.quantity = quantity;
+//       // Update the quantity
+//       existingItem.quantity = quantity;
 
-      const updatedCart = await userCart.save();
-      res.json(updatedCart);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
+//       const updatedCart = await userCart.save();
+//       res.json(updatedCart);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
 
-  // Remove an item from the cart
-  removeItemFromCart: async (req, res) => {
-    const userId = req.user.userId;
-    const { productId } = req.params;
+//   // Remove an item from the cart
+//   removeItemFromCart: async (req, res) => {
+//     const userId = req.user.userId;
+//     const { productId } = req.params;
 
-    try {
-      const userCart = await Cart.findOne({ user: userId });
-      if (!userCart) {
-        return res.status(404).json({ error: 'Cart not found' });
-      }
+//     try {
+//       const userCart = await Cart.findOne({ user: userId });
+//       if (!userCart) {
+//         return res.status(404).json({ error: 'Cart not found' });
+//       }
 
-      // Remove the item from the cart
-      userCart.items = userCart.items.filter((item) => item.product.toString() !== productId);
+//       // Remove the item from the cart
+//       userCart.items = userCart.items.filter((item) => item.product.toString() !== productId);
 
-      const updatedCart = await userCart.save();
-      res.json(updatedCart);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
-};
+//       const updatedCart = await userCart.save();
+//       res.json(updatedCart);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
+// };
 
-module.exports = cartController;
+// module.exports = cartController;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,116 +1,116 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const User = require('../models/User');
-const secret = "mysecretsshhhhh";
+// const bcrypt = require('bcrypt');
+// const jwt = require('jsonwebtoken');
+// const User = require('../models/User');
+// const secret = "mysecretsshhhhh";
 
-const userController = {
-  // Register a new user
-  registerUser: async (req, res) => {
-    try {
-      const { username, password } = req.body;
+// const userController = {
+//   // Register a new user
+//   registerUser: async (req, res) => {
+//     try {
+//       const { username, password } = req.body;
 
-      // Check if the username is already taken
-      const existingUser = await User.findOne({ username });
-      if (existingUser) {
-        return res.status(400).json({ error: 'Username is already taken' });
-      }
+//       // Check if the username is already taken
+//       const existingUser = await User.findOne({ username });
+//       if (existingUser) {
+//         return res.status(400).json({ error: 'Username is already taken' });
+//       }
 
-      // Hash the password before saving it to the database
-      const hashedPassword = await bcrypt.hash(password, 10);
+//       // Hash the password before saving it to the database
+//       const hashedPassword = await bcrypt.hash(password, 10);
 
-      const newUser = new User({
-        username,
-        password: hashedPassword,
-      });
+//       const newUser = new User({
+//         username,
+//         password: hashedPassword,
+//       });
 
-      await newUser.save();
+//       await newUser.save();
 
-      res.status(201).json({ success: true, message: 'User registered successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
+//       res.status(201).json({ success: true, message: 'User registered successfully' });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
 
-  // Login user
-  loginUser: async (req, res) => {
-    try {
-      const { username, password } = req.body;
+//   // Login user
+//   loginUser: async (req, res) => {
+//     try {
+//       const { username, password } = req.body;
 
-      const user = await User.findOne({ username });
-      if (!user) {
-        return res.status(401).json({ error: 'User not found' });
-      }
+//       const user = await User.findOne({ username });
+//       if (!user) {
+//         return res.status(401).json({ error: 'User not found' });
+//       }
 
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return res.status(401).json({ error: 'Invalid password' });
-      }
+//       const isPasswordValid = await bcrypt.compare(password, user.password);
+//       if (!isPasswordValid) {
+//         return res.status(401).json({ error: 'Invalid password' });
+//       }
 
-      // Generate JWT token
-      const token = jwt.sign({ userId: user._id, username: user.username }, secret, {
-        expiresIn: '1h', // You can set the expiration time as needed
-      });
+//       // Generate JWT token
+//       const token = jwt.sign({ userId: user._id, username: user.username }, secret, {
+//         expiresIn: '1h', // You can set the expiration time as needed
+//       });
 
-      res.json({ success: true, token });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
+//       res.json({ success: true, token });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
 
-  // Get user profile
-  getUserProfile: async (req, res) => {
-    try {
-      // Fetch user data from the authenticated user
-      const user = await User.findById(req.user.userId);
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
+//   // Get user profile
+//   getUserProfile: async (req, res) => {
+//     try {
+//       // Fetch user data from the authenticated user
+//       const user = await User.findById(req.user.userId);
+//       if (!user) {
+//         return res.status(404).json({ error: 'User not found' });
+//       }
 
-      // Exclude sensitive information (like password) before sending the response
-      const userProfile = {
-        username: user.username,
-        // Add other user-related fields...
-      };
+//       // Exclude sensitive information (like password) before sending the response
+//       const userProfile = {
+//         username: user.username,
+//         // Add other user-related fields...
+//       };
 
-      res.json(userProfile);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
+//       res.json(userProfile);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
 
-  // Update user profile
-  updateUserProfile: async (req, res) => {
-    try {
-      const { username, newPassword } = req.body;
+//   // Update user profile
+//   updateUserProfile: async (req, res) => {
+//     try {
+//       const { username, newPassword } = req.body;
 
-      // Fetch user data from the authenticated user
-      const user = await User.findById(req.user.userId);
-      if (!user) {
-        return res.status(404).json({ error: 'User not found' });
-      }
+//       // Fetch user data from the authenticated user
+//       const user = await User.findById(req.user.userId);
+//       if (!user) {
+//         return res.status(404).json({ error: 'User not found' });
+//       }
 
-      // Update username if provided
-      if (username) {
-        user.username = username;
-      }
+//       // Update username if provided
+//       if (username) {
+//         user.username = username;
+//       }
 
-      // Update password if provided
-      if (newPassword) {
-        const hashedPassword = await bcrypt.hash(newPassword, 10);
-        user.password = hashedPassword;
-      }
+//       // Update password if provided
+//       if (newPassword) {
+//         const hashedPassword = await bcrypt.hash(newPassword, 10);
+//         user.password = hashedPassword;
+//       }
 
-      await user.save();
+//       await user.save();
 
-      res.json({ success: true, message: 'User profile updated successfully' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  },
-};
+//       res.json({ success: true, message: 'User profile updated successfully' });
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'Internal Server Error' });
+//     }
+//   },
+// };
 
-module.exports = userController;
+// module.exports = userController;
